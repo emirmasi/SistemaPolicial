48c19691d87d17cbfe66d277c01c49c3ad1883a7
emirmasi <emir12345masi@gmail.com>
2025-10-25 20:54:07 -0300

    add hilt and dagger

---
+++ app/src/main/java/com/practica/policeubgapp/PoliceAplication.kt
@@ -0,0 +1,7 @@
+package com.practica.policeubgapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PoliceApplication : Application()
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
+/*plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.service) apply false
+    alias(libs.plugins.daggerHiltAndroid) apply false
+    alias(libs.plugins.ksp) apply false
+}
+
+ */
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.google.services) apply false
 }

fa44115064900f4bb9f2b5b07eafbdb4b9759427
emirmasi <emir12345masi@gmail.com>
2026-01-26 17:22:32 -0300

    arregle los problemas de inicio de sesion

--- app/src/main/java/com/practica/policeubgapp/ui/screens/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/practica/policeubgapp/ui/screens/splashScreen/SplashScreen.kt
@@ -1,29 +1,60 @@
 package com.practica.policeubgapp.ui.screens.splashScreen
 
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.practica.policeubgapp.R
+import com.practica.policeubgapp.data.location.LocationService
 import com.practica.policeubgapp.ui.navigations.NavigationRoutes
-import com.practica.policeubgapp.ui.screens.splashScreen.SplashViewModel
+import com.practica.policeubgapp.ui.navigations.isServiceRunning
 
 @Composable
 fun SplashScreen(navHostController: NavHostController) {
+    val context = LocalContext.current
     val splashVm: SplashViewModel = hiltViewModel()
 
-    val isUserAuth = splashVm.isUserAuthenticated.collectAsState()
-    LaunchedEffect(isUserAuth.value) {
-        if (isUserAuth.value == null) return@LaunchedEffect
+    val isUserAuth by splashVm.isUserAuthenticated.collectAsState()
 
-        if (isUserAuth.value == true) {
-            navHostController.navigate(route = NavigationRoutes.MainScreen.route) {
-                popUpTo(NavigationRoutes.Splash.route) { inclusive = true }
+    LaunchedEffect(isUserAuth) {
+        Log.d("SPLASH", "Estado de Auth: $isUserAuth")
+        if (isUserAuth == null) return@LaunchedEffect
+
+        if (isUserAuth == true) {
+            Log.d("SPLASH", "Navegando a Main")
+            if (isServiceRunning(context, LocationService::class.java)) {
+                Log.d("SPLASH", "el servicio de location esta activado")
+                navHostController.navigate(route = NavigationRoutes.MainScreen.route) {
+                    popUpTo(NavigationRoutes.Splash.route) { inclusive = true }
+                }
+            }else{
+                Log.d("SPLASH", "el servicio de location no esta activado")
+                navHostController.navigate(route = NavigationRoutes.MainScreen.route) {
+                    popUpTo(NavigationRoutes.Splash.route) { inclusive = true }
+                }
             }
         } else {
-            navHostController.navigate(route = NavigationRoutes.Login.route) {
+            Log.d("SPLASH", "no hay nadie autenticado user es null")
+            navHostController.navigate(route = NavigationRoutes.Login.route){
                 popUpTo(NavigationRoutes.Splash.route) { inclusive = true }
             }
         }
+
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(painter = painterResource(id = R.drawable.ciudad_logo1), contentDescription = null)
     }
 }

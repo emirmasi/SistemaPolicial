e9525ef15f41e4ab5883bbd96d323d5a34cc0280
emirmasi <emir12345masi@gmail.com>
2026-05-08 22:51:49 -0300

    se mejoro el detalle del servicio

--- app/src/main/java/com/practica/policeubgapp/ui/components/ServiceDetailSheet.kt
+++ app/src/main/java/com/practica/policeubgapp/ui/components/ServiceDetailSheet.kt
@@ -1,33 +1,50 @@
 package com.practica.policeubgapp.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.practica.policeubgapp.R
 import com.practica.policeubgapp.domain.models.PendingServiceUI
+import com.practica.policeubgapp.domain.models.SCHEDULE
+import com.practica.policeubgapp.domain.models.TYPESERVICE
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ServiceDetailSheet(
@@ -39,68 +56,116 @@ fun ServiceDetailSheet(
     ModalBottomSheet(
         onDismissRequest = onDismiss,
         sheetState = sheetState,
-        containerColor = Color.White,
-        dragHandle = { BottomSheetDefaults.DragHandle() }, // La barrita para deslizar
-        scrimColor = Color.Black.copy(alpha = 0.4f) // El fondo oscurecido
+        containerColor = MaterialTheme.colorScheme.background,
+        scrimColor = Color.Black.copy(alpha = 0.6f), // El fondo oscurecido
     ) {
         // Contenido del detalle (Elevado)
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 20.dp)
-                .padding(bottom = 32.dp)
+                .padding(bottom = 8.dp)
+                .background(MaterialTheme.colorScheme.background),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = service.typeService.name,
-                style = MaterialTheme.typography.headlineSmall,
-                fontWeight = FontWeight.Bold
-            )
-
-            Spacer(modifier = Modifier.height(12.dp))
-
-            // Mini Mapa Estático
+            /// Drag handle personalizo
+//            Box(
+//                modifier = Modifier
+//                    .padding(vertical = 12.dp)
+//                    .width(40.dp)
+//                    .height(4.dp)
+//                    .background(Color.LightGray, RoundedCornerShape(2.dp))
+//            )
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(150.dp),
-                shape = RoundedCornerShape(16.dp)
+                    .padding(horizontal = 12.dp)
+                    .padding(bottom = 12.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)
             ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color(0xFFF0F0F0)),
-                    contentAlignment = Alignment.Center
+                Column(
+                    modifier = Modifier.padding(16.dp)
                 ) {
                     Text(
-                        "Mapa de ${service.locationName}",
-                        modifier = Modifier.padding(top = 40.dp)
+                        text = "DETALLES DEL SERVICIO",
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = MaterialTheme.colorScheme.onPrimary
                     )
-                    MiniMapaEstatico(service.location)
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = service.typeService.name,
+                            style = MaterialTheme.typography.headlineMedium,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                        Spacer(modifier = Modifier.width(6.dp))
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_check_circle_24),
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.onPrimary,
+                            modifier = Modifier.size(18.dp)
+                        )
+                    }
+                }
+            }
+            ///-DATOS DETALLADO DEL SERVICIO ---
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
+                border = BorderStroke(1.dp, Color.LightGray)
+            ) {
+                Column(
+                    modifier = Modifier.padding(20.dp)
+                ) {
+                    DataRowWithIcon(
+                        icon = painterResource(id = R.drawable.baseline_calendar_month_24),
+                        label = "FECHA",
+                        value = service.date
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    DataRowWithIcon(
+                        icon = painterResource(id = R.drawable.baseline_hourglass_bottom_24),
+                        label = "HORARIO",
+                        value = service.schedule.timeRange
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    DataRowWithIcon(
+                        icon = painterResource(id = R.drawable.baseline_place_24),
+                        label = "DIRECCIÓN",
+                        value = service.locationName
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(160.dp),
+                        shape = RoundedCornerShape(12.dp),
+                    ) {
+                        MiniMapaEstatico(service.location)
+                    }
                 }
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Datos rápidos
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                InfoColumn("Horario", service.schedule.timeRange)
-                InfoColumn(" Direccion", service.locationName)
-            }
-
-            Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.padding(8.dp))
 
             Button(
                 onClick = onStartService,
                 modifier = Modifier
                     .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
                     .height(56.dp),
                 shape = RoundedCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2E7D32))
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
             ) {
-                Text("INICIAR SERVICIO ACTUAL", fontSize = 16.sp)
+                Text("INICIAR SERVICIO ACTUAL",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
             }
         }
     }
@@ -108,10 +173,49 @@ fun ServiceDetailSheet(
 }
 
 @Composable
-fun InfoColumn(label: String, value: String) {
-    Row(modifier = Modifier.padding(vertical = 4.dp)) {
-        Text(text = "$label: ", fontWeight = FontWeight.Bold)
-        Text(text = value)
+fun DataRowWithIcon(icon: Painter, label: String, value: String) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            painter = icon,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(20.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = "$label: ",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 14.sp
+        )
+        Text(
+            text = value,
+            color = Color.DarkGray,
+            fontSize = 14.sp
+        )
+
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun ServiceDetailSheetPreview(){
+  val sheetState = rememberModalBottomSheetState()
+    var showSheet by remember { mutableStateOf(false) }
+    ServiceDetailSheet(
+        service = PendingServiceUI(
+            uid = "1",
+            lp = 6252,
+            typeService = TYPESERVICE.UBG,
+            locationName = "av segurola y la plata",
+            location = null,
+            schedule = SCHEDULE.TARDE,
+            date = "20/05/2021"
+        ),
+        sheetState = sheetState,
+        onDismiss = {showSheet = true},
+        onStartService = {}
+    )
+        
+}
